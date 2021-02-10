b8acfbf7232d65f2632da064a8626c5b8f534df9
Eric Schultz <eschultz591@pop-os.localdomain>
2021-02-09 20:25:39 -0600

    my changes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
 
-//import TestGame from './TestGame.js'
+
+// Get started by creating a new React 
+// component and importing the libraries!
+
+import React from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    // Next up create a new Unity Context object to 
+    // initialise and define your WebGL build. The 
+    // paths are relative from your index file.
 
+    this.unityContext = new UnityContext({
+      loaderUrl: "build/build_1/Build/build_1.loader.js",
+      dataUrl: "build/build_1/Build/build_1.data",
+      frameworkUrl: "build/build_1/Build/build_1.framework.js",
+      codeUrl: "build/build_1/Build/build_1.wasm",
+    });
+  }
 
+  render() {
 
+    // Finally render the Unity component and pass
+    // the Unity context through the props.
 
-const unityContext = new UnityContext({
-  loaderUrl: "build/build_2/Build/build_2.loader.js",
-  dataUrl: "build/build_2/Build/build_2.data.gz",
-  frameworkUrl: "build/build_2/Build/build_2.framework.js.gz",
-  codeUrl: "build/build_2/Build/build_2.wasm.gz",
-});
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Unity unityContext={unityContext} />;
-
-    </div>
-
-    
-  );
+    return <Unity unityContext={this.unityContext} />;
+  }
 }
-
-
-
-
 
 export default App;
