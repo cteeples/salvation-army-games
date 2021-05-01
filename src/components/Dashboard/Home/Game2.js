107afc4524c4b44de95eda5e36dd694d86d1e283
Jamie Nguyen <monkeyah2@gmail.com>
2021-04-30 20:15:08 -0500

    add Electric Circuit Game
    can pick/place/delete/rotate tiles

--- src/components/Dashboard/Home/Game2.js
+++ src/components/Dashboard/Home/Game2.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { Link, useHistory } from "react-router-dom"
+import Unity, { UnityContext } from "react-unity-webgl";
 
 export default function Game2() {
-    return(
-    <Container className="ml-5">
-        game 2
-    </Container>
-    ) 
+    return (
+        <Container className="ml-5">
+            <Unity unityContext={unityContext}
+                matchWebGLToCanvasSize={true}
+                style={{ width:"1280px", height:"720px" }}
+
+            />
+        </Container>
+
+    )
 }
+
+
+
+const unityContext = new UnityContext({
+    codeUrl: "/buildEC/EC.wasm",
+    frameworkUrl: "/buildEC/EC.framework.js",
+    dataUrl: "/buildEC/EC.data",
+    loaderUrl: "/buildEC/EC.loader.js"
+});
